c817841d8fd5c2b7bc9e8adf09bf26a4a8623730
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2022-11-16 20:42:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+win2=window.open("https://box-testing-account.github.io/spencer")
 
+function reloadwin2(){
+    if (!win2.closed)
+        win2.location.reload()
+    else
+        return
+    setTimeout("reloadwin2()",1000)
+}
+reloadwin2()
