2d223fbaca30b858cc7d31bc39c03ad1f0d55de4
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2022-11-17 09:31:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-win2=window.open("https://box-testing-account.github.io/Spencer")
+//win2=window.open("https://box-testing-account.github.io/Spencer")
+//
+//function reloadwin2(){
+//    if (!win2.closed) {
+//        win2=window.open("https://box-testing-account.github.io/Spencer")
+//    }
+//    else{
+//        return
+//    setTimeout("reloadwin2()",1000)
+//    }
+//}
+//reloadwin2()
 
-function reloadwin2(){
-    if (!win2.closed) {
-        win2=window.open("https://box-testing-account.github.io/Spencer")
-    }
-    else{
-        return
-    setTimeout("reloadwin2()",1000)
-    }
-}
-reloadwin2()
+// Open a new browser window
+const openedWindow = window.open("https://box-testing-account.github.io/Spencer");
+if (opendedWindow.closed == 'false') {
+    console.log("it is open");
+} else {
+    const openedWindow = window.open("https://box-testing-account.github.io/Spencer");
+// check if the window is in opened or closed state
+console.log(openedWindow.closed); // false
