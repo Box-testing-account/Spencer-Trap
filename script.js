9a7d8e238dc889863f483308403196a1aaeed68a
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-15 06:11:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let spencer
 spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
 
-//document.addEventListener("visibilitychange", function() {
-//    if (document.visibilityState === "visible") {
-//      window.open("https://box-testing-account.github.io/Spencer")
-//    }
-//});
-
 setInterval(function(){ 
   if(spencer.closed){
     console.log("Tab is closed");
@@ -14,26 +8,4 @@ setInterval(function(){
   }else{
     console.log("Tab is open")
   }
-}, 100);
-
-//setInterval(function() {
-//   if(localStorage.getItem('tabIsOpen') === 'closed'){
-//        console.log("Another tab has been closed");
-//        spencer.close();
-//        spencer = window.open("https://box-testing-account.github.io/Spencer");
-//        // perform necessary actions here
-//   } else {
-//        window.onfocus = function() {
-//            spencer = window.open("https://box-testing-account.github.io/Spencer");
-//        };
-//   };
-//}, 100);
-
-// Open a new browser window
-//const openedWindow = window.open("https://box-testing-account.github.io/Spencer");
-//if (opendedWindow.closed == 'false') {
-//    console.log("it is open");
-//} else {
-//    const openedWindow = window.open("https://box-testing-account.github.io/Spencer");
-// check if the window is in opened or closed state
-//console.log(openedWindow.closed); // false
+}, 50);
