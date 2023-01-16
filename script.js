ced23d6b4f1a3c27671a57810c0a91b8254e341b
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-16 10:44:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
-setInterval(() => {
-    localStorage.getItem("activated") === "true" && (spencer.closed && (spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0')),null) || (window.close(),null)
+let spencer
+spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
+
+setInterval(function() {
+  if (localStorage.getItem("activated") === "true") {
+    if(spencer.closed){
+      console.log("Tab is closed");
+      spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
+    };
+    console.log("Activated is true");
+  } else {
+    // Code to run if "activated" is false
+    console.log("Activated is false");
+    window.close();
+  }
 }, 50);
