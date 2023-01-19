eebab1465d420a0629f25b00eaca97d83cf5ffef
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-19 08:46:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank",
 setInterval(function() {
   if (localStorage.getItem("activated") === "true") {
     if(spencer.closed){
-      console.log("Tab is closed");
       spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
     };
-    console.log("Activated is true");
   } else {
-    // Code to run if "activated" is false
-    console.log("Activated is false");
     window.close();
   }
 }, 50);
